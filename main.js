b094be3ef5b6f75f24e43ee153a19a82c03eef7f
Farnerud <hperea.alfa@gmail.com>
2021-07-10 10:56:58 -0500

    L2-start | v-for directive.

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
       image: "./assets/images/socks_green.jpg",
       inventory: 100,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% poliester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: [28, 30, 32, 34],
     };
   },
 });
